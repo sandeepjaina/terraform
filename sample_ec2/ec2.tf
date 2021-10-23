ebf98dc52abacbe66a840de2e60c9703a71d3260
sandeepjaina <sandeep.jaina@outlook.com>
2021-10-23 09:28:29 -0700

    Initial commit

--- sample_ec2/ec2.tf
+++ sample_ec2/ec2.tf
@@ -21,7 +21,7 @@ resource "aws_security_group" "allow_tls" {
   egress = [
     {
       from_port        = 0
-      to_port          = 0
+      to_port          = 65535
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
     }
