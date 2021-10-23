b92b7e99b13a97668733dc19d09bb5667d4fc345
sandeepjaina <sandeep.jaina@outlook.com>
2021-10-23 09:51:09 -0700

    Initial commit

--- sample_ec2/ec2.tf
+++ sample_ec2/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "sample" {
-  ami                   = "ami-074df373d6bafa625"
+  ami                   = "ami-0e4e4b2f188e91845"
   instance_type         = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
