fe189baac7999df4a6d3783278db956a9e1dea7c
sandeepjaina <sandeep.jaina@outlook.com>
2021-10-23 09:22:01 -0700

    Initial commit

--- sample_ec2/ec2.tf
+++ sample_ec2/ec2.tf
@@ -23,7 +23,6 @@ resource "aws_security_group" "allow_tls" {
       to_port          = 0
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
     }
   ]
 
