b381d8c7264c02ed3ab6de0eaf8bb24978c27d94
sandeepjaina <sandeep.jaina@outlook.com>
2021-10-24 01:25:05 -0700

    Initial commit

--- remote-statefile/ec2.tf
+++ remote-statefile/ec2.tf
@@ -14,6 +14,6 @@ terraform {
    bucket = "sjaina-terraform"
    key = "sample/terraform.tfstate"
    region = "us-east-1"
-   dynamodb_table = "terraform"
+   dynamodb_table = "terraform_state"
  }
 }
