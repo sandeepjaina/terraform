83e5e8e641d6ca2fdb472479003ed0335b895533
sandeepjaina <sandeep.jaina@outlook.com>
2021-10-24 02:29:29 -0700

    Initial commit

--- Module/vpc/vpc.tf
+++ Module/vpc/vpc.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1b"
 
   tags = {
     Name = "tf-example"
