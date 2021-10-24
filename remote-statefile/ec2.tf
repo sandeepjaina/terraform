e645edc873fc9c088d22b80d12e8fba30a4ee8eb
sandeepjaina <sandeep.jaina@outlook.com>
2021-10-24 00:43:40 -0700

    Initial commit

---
+++ remote-statefile/ec2.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "sample" {
+  ami           = "ami-0e4e4b2f188e91845"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "statefile"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+terraform {
+ backend "s3" {
+   bucket = "sjaina-terraform"
+   key = "/sample/terraform.tfstate"
+   region = "us-east-1"
+ }
+}
