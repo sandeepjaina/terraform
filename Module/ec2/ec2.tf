58b4271e058cfecafcfe97a7c2967c0c07fba694
sandeepjaina <sandeep.jaina@outlook.com>
2021-10-24 03:10:04 -0700

    Initial commit

--- Module/ec2/ec2.tf
+++ Module/ec2/ec2.tf
@@ -2,17 +2,15 @@ resource "aws_instance" "foo" {
   count = 2
   ami           = "ami-0e4e4b2f188e91845" # us-west-2
   instance_type = "t2.micro"
-
-  network_interface {
-    device_index         = 0
-    network_interface_id = aws_network_interface.interface.id
-  }
+tags = {
+  Name = "example-{count.index}"
+}
 
 
 }
 
 resource "aws_network_interface" "interface" {
-  count = 2
+  #count = 2
   subnet_id = var.subnet
   }
 
