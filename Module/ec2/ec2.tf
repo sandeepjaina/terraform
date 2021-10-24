ab6f5e86736b455054a5c8961e90ae1314128762
sandeepjaina <sandeep.jaina@outlook.com>
2021-10-24 02:59:37 -0700

    Initial commit

--- Module/ec2/ec2.tf
+++ Module/ec2/ec2.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "foo" {
+  count = 2
   ami           = "ami-0e4e4b2f188e91845" # us-west-2
   instance_type = "t2.micro"
 
@@ -11,6 +12,7 @@ resource "aws_instance" "foo" {
 }
 
 resource "aws_network_interface" "interface" {
+  count = 2
   subnet_id = var.subnet
   }
 
