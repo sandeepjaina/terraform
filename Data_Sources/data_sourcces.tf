9b7e705ac72619a7d114a3da7af042c9d481d4db
sandeepjaina <sandeep.jaina@outlook.com>
2021-10-25 05:35:44 -0700

    Initial commit

--- Data_Sources/data_sourcces.tf
+++ Data_Sources/data_sourcces.tf
@@ -14,10 +14,14 @@ provider "aws" {
 
 data "aws_ec2_spot_price" "example" {
   instance_type     = "t2.micro"
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
 
   filter {
     name   = "product-description"
     values = ["Linux/UNIX"]
   }
+}
+
+output "spot_price" {
+  value = data.aws_ec2_spot_price.example.spot_price
 }
