74b40e9acc4988dffc712f7ff3ba504cd9a9a044
sandeepjaina <sandeep.jaina@outlook.com>
2021-10-25 04:23:54 -0700

    Initial commit

--- Data_Sources/data_sourcces.tf
+++ Data_Sources/data_sourcces.tf
@@ -1,9 +1,11 @@
 data "aws_ami" "ami_name" {
   owners = [973714476881]
+  most_recent = true
+  name_regex       = "^centos"
 }
 
 output "sa" {
-  value = data.aws_ami.ami_name
+  value = data.aws_ami.ami_name.id
 }
 
 provider "aws" {
