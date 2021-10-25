e87527a670263c1cc990c7f5529803c1717e82d0
sandeepjaina <sandeep.jaina@outlook.com>
2021-10-25 06:46:14 -0700

    Initial commit

--- multi_env/ec2.tf
+++ multi_env/ec2.tf
@@ -2,6 +2,8 @@ resource "aws_instance" "foo" {
   ami           = "ami-0e4e4b2f188e91845" # us-west-2
   instance_type = "t2.micro"
   tags          = {
-    Name = "SampleVM-${ENV}"
+    Name = "SampleVM-${var.ENV}"
   }
 }
+
+variable "ENV" {}
