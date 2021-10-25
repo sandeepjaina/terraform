784f20226a103355b1ba6b35ca0e3be13b80aa91
sandeepjaina <sandeep.jaina@outlook.com>
2021-10-24 22:48:26 -0700

    Initial commit

--- roboshop/roboshop.tf
+++ roboshop/roboshop.tf
@@ -1,5 +1,6 @@
-resource "aws_instance" "roboshop" {
+resource "aws_spot_instance_request" "roboshop" {
   count = length(var.components)
+  spot_price    = "0.031"
   ami = "ami-0e4e4b2f188e91845"
   instance_type = "t2.micro"
 
