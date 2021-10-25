b31f7996113b4bda3da0ec83cbccbaaf526bb47e
sandeepjaina <sandeep.jaina@outlook.com>
2021-10-25 02:55:58 -0700

    Initial commit

--- roboshop/roboshop.tf
+++ roboshop/roboshop.tf
@@ -30,24 +30,25 @@ resource "aws_ec2_tag" "tagname" {
 }
 
 
-#resource "null_resource" "provisioning" {
-#  count = length(var.components)
-#
-#  provisioner "remote-exec" {
-#    connection {
-#      host = element(aws_spot_instance_request.roboshop.*.public_ip, count.index)
-#      user = "centos"
-#      password = "DevOps321"
-#    }
-#    inline = [
-#      "cd /home/centos/",
-#      "git clone https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps57/_git/shell-scripting",
-#      "cd shell-scripting/roboshop",
-#      "sudo set-hostname ${element(var.components, count.index)}"
-#    ]
-#
-#  }
-#}
+resource "null_resource" "provisioning" {
+  depends_on = [aws_route53_record.route]
+  count = length(var.components)
+
+  provisioner "remote-exec" {
+    connection {
+      host = element(aws_spot_instance_request.roboshop.*.public_ip, count.index)
+      user = "centos"
+      password = "DevOps321"
+    }
+    inline = [
+      "cd /home/centos/",
+      "git clone https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps57/_git/shell-scripting",
+      "cd shell-scripting/roboshop",
+      "sudo set-hostname ${element(var.components, count.index)}"
+    ]
+
+  }
+}
 
 resource "aws_route53_record" "route" {
   count = local.LENGTH
