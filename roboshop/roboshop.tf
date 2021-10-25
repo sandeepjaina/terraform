870f427818d0090606fc9399a7d6ede1d2fb9914
sandeepjaina <sandeep.jaina@outlook.com>
2021-10-25 00:33:06 -0700

    Initial commit

--- roboshop/roboshop.tf
+++ roboshop/roboshop.tf
@@ -41,6 +41,6 @@ resource "aws_ec2_tag" "tagname" {
 #}
 
 output "securitygroups" {
-  value = aws_spot_instance_request.roboshop.security_groups
+  value = aws_spot_instance_request.roboshop.*.security_groups
 }
 variable "components" {}
